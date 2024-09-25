e8f179abced05d0cb1e9d923066ae6d3bfdde756
OJJProjects <oscarjoneslaptop@gmail.com>
2024-09-25 19:14:48 +0100

    Added the base code to the program. This incudes the basic GUI which I have created previously before this GitHub repo.

--- Source Code/main.py
+++ Source Code/main.py
@@ -1,1 +1,119 @@
+#Import required libraries
+import customtkinter
+import tkinter as tk
+from tkinter import ttk
+import pyodbc as pyo
+import configparser
 
+# Functions
+def on_submit_click():
+	cnn_string = (
+		r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+		r"DBQ=C:\Users\oscar\Documents\VSCode Projects\flightlogbook\database.accdb"
+		)
+	cnn = pyo.connect(cnn_string)
+	cursor = cnn.cursor()
+	cursor.execute(
+		"INSERT INTO flight_data VALUES	 (?,?,?,?,?,?,?)",
+		(flight_number_entry.get(),aircraft_entry.get(),departure_airport_entry.get(),arrival_airport_entry.get(),departure_time_entry.get(),arrival_time_entry.get(),duration_entry.get())
+
+	)
+	cursor.commit()
+
+def change_resolution(choice):
+	if choice == "3840x2160":
+		root.geometry("3840x2160")
+	if choice == "2560x1440":
+		root.geometry("2560x1440")
+	if choice == "1920x1080":
+		root.geometry("1920x1080")
+	if choice == "800x500":
+		root.geometry("800x500")
+	global_choice = choice
+
+def dark_mode_switch():
+	variable = dark_mode_variable.get()
+	if variable == "on":
+		customtkinter.set_appearance_mode("dark")
+	if variable == "off":
+		customtkinter.set_appearance_mode("light")
+
+############## GUI SECTION ##############
+# Get settings from .ini file
+
+
+root=customtkinter.CTk()
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+customtkinter.set_window_scaling(1.0)
+customtkinter.set_widget_scaling(1.0)
+
+root.title("Flight Logbook")
+root.geometry("800x500")
+
+my_tabs = customtkinter.CTkTabview(root)
+my_tabs.pack(pady=10)
+tab_create_new_flight = my_tabs.add("New Flight")
+tab_view_past_flights = my_tabs.add("Past Flights")
+tab_settings = my_tabs.add("Settings")
+
+# Create new flight section
+flight_number_entry = customtkinter.CTkEntry(tab_create_new_flight, placeholder_text="Flight Number")
+flight_number_entry.pack(pady=5)
+
+aircraft_entry = customtkinter.CTkEntry(tab_create_new_flight, placeholder_text="Aircraft")
+aircraft_entry.pack(pady=5)
+
+departure_airport_entry = customtkinter.CTkEntry(tab_create_new_flight, placeholder_text="Departure Airport")
+departure_airport_entry.pack(pady=5)
+
+arrival_airport_entry = customtkinter.CTkEntry(tab_create_new_flight, placeholder_text="Arrival Airport")
+arrival_airport_entry.pack(pady=5)
+
+departure_time_entry = customtkinter.CTkEntry(tab_create_new_flight, placeholder_text="Departure Time")
+departure_time_entry.pack(pady=5)
+
+arrival_time_entry = customtkinter.CTkEntry(tab_create_new_flight, placeholder_text="Arrival Time")
+arrival_time_entry.pack(pady=5)
+
+duration_entry = customtkinter.CTkEntry(tab_create_new_flight, placeholder_text="Duration Time")
+duration_entry.pack(pady=5)
+
+submit_button = customtkinter.CTkButton(tab_create_new_flight, text="Save", command=on_submit_click)
+submit_button.pack(pady=5)
+
+# Past Flights Section
+table = ttk.Treeview(tab_view_past_flights, columns = ("treeview_flight_number","treeview_aircraft","treeview_departure_aiport","treeview_arrival_airport","treeview_departure_time","treeview_arrival_time","treeview_duration"), show = "headings")
+table.heading("treeview_flight_number", text="Flight Number")
+table.heading("treeview_aircraft", text="Aircraft")
+table.heading("treeview_departure_aiport", text="Departure Airport")
+table.heading("treeview_arrival_airport", text="Arrival Airport")
+table.heading("treeview_departure_time", text="Departure Time")
+table.heading("treeview_arrival_time", text="Arrival Time")
+table.heading("treeview_duration", text="Duration")
+table.pack()
+
+cnn_string = (
+		r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+		r"DBQ=C:\Users\oscar\Documents\VSCode Projects\flightlogbook\database.accdb"
+		)
+cnn = pyo.connect(cnn_string)
+cursor = cnn.cursor()
+cursor.execute("SELECT * FROM flight_data")
+for row in cursor:
+	table.insert('','end', value=row[0:7])
+
+# Settings Section
+dark_mode_variable = customtkinter.StringVar(value="on")
+dark_mode_switch = customtkinter.CTkSwitch(tab_settings,text="Dark Mode",variable=dark_mode_variable,onvalue="on",offvalue="off",command=dark_mode_switch)
+dark_mode_switch.pack(pady=10)
+
+resolution_options=["3840x2160","2560x1440","1920x1080","800x500"]
+resolution_combo_box = customtkinter.CTkComboBox(tab_settings, values=resolution_options, command=change_resolution)
+resolution_combo_box.pack(pady=5)
+
+settings_save_button = customtkinter.CTkButton(tab_settings, text="Save")
+settings_save_button.pack(pady=5)
+
+
+root.mainloop()
